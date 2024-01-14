488b12a603ac3ce0fe09e595aca37ceca3693b72
zqywuxie <573905257@qq.com>
2024-01-14 22:02:35 +0800

    接入支付宝功能(沙盒)

--- src/main/java/com/example/studypartner/config/WebMvcConfig.java
+++ src/main/java/com/example/studypartner/config/WebMvcConfig.java
@@ -4,19 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- *
- */
-@Configuration // 一定不要忽略此注解
+
+
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        // 所有接口
-        registry.addMapping("/**")
-                .allowCredentials(true)
-                .allowedOrigins("*")
-                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
-                .allowedHeaders("*")
-                .maxAge(3600);
-    }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// 所有接口
+		registry.addMapping("/**")
+				.allowCredentials(true)
+				.allowedOrigins("*")
+				.allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+				.allowedHeaders("*")
+				.maxAge(3600);
+	}
 }
